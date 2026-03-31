327be45e63d382bb65128996e3b958b3b4bf4e25
brenolg <brenolgar@gmail.com>
2026-03-31 15:31:55 -0300

    feat: initial commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Bem-vindo à página inicial</p>
+      <div className="bg-green-500 text-white p-4">Tailwind funcionando</div>
+    </div>
+  );
+}
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Faça seu login aqui</p>
+    </div>
+  );
+}
